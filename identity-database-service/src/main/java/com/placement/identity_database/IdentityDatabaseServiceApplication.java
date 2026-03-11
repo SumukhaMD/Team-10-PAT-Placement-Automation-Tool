82118401e4938f0a8984fc308ed768c6733673a4
Vikas N <vikasnagabhushan12@gmail.com>
2026-03-11 09:22:11 +0530

    Added identity database service

---
+++ identity-database-service/src/main/java/com/placement/identity_database/IdentityDatabaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.placement.identity_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityDatabaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityDatabaseServiceApplication.class, args);
+	}
+
+}
