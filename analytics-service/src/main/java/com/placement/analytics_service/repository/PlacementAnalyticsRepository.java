3fc72de27d807ebe2e1036469444e23f92620278
shreyas162424 <savitasangalad@gmail.com>
2026-03-23 13:54:30 +0530

    Added quries in repository

--- analytics-service/src/main/java/com/placement/analytics_service/repository/PlacementAnalyticsRepository.java
+++ analytics-service/src/main/java/com/placement/analytics_service/repository/PlacementAnalyticsRepository.java
@@ -7,4 +7,16 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PlacementAnalyticsRepository extends JpaRepository<PlacementAnalytics, Long> {
 
+    @Query("SELECT COUNT(p) FROM PlacementAnalytics p WHERE p.status='PLACED'")
+    Long getTotalPlacedStudents();
+    
+    @Query("SELECT AVG(p.salaryPackage) FROM PlacementAnalytics p WHERE p.status='PLACED'")
+    Double getAveragePackage();
+    
+    @Query("SELECT MAX(p.salaryPackage) FROM PlacementAnalytics p WHERE p.status='PLACED'")
+    Double getHighestPackage();
+    
+    @Query("SELECT (COUNT(p) * 100.0 / (SELECT COUNT(p2) FROM PlacementAnalytics p2)) FROM PlacementAnalytics p WHERE p.status='PLACED'")
+    Double getPlacementPercentage();
+
 }
