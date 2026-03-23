ef010fa41e0e6634712c4fc3cf2db8e8caeffd0c
shreyas162424 <savitasangalad@gmail.com>
2026-03-23 13:56:08 +0530

    Added dto layer

---
+++ analytics-service/src/main/java/com/placement/analytics_service/dto/PlacementStatsDTO.java
@@ -0,0 +1,11 @@
+package com.placement.analytics_service.dto;
+
+public class PlacementStatsDTO {
+
+    private Long totalPlaced;
+    private Double averagePackage;
+    private Double highestPackage;
+    private Double placementPercentage;
+
+    // getters & setters
+}
